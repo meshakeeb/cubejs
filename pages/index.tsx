a20b8bb934337dd6b8cde5ca8bb3bdc8575c17f9
meshakeeb <me@shakeebahmed.com>
2022-03-14 09:23:29 +0300

    Tailwind integrated

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,9 @@ import Layout from '../components/Layout'
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋</h1>
+	<h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     <p>
       <Link href="/about">
         <a>About</a>
